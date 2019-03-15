d2957ac759425cc3edeec9a9bac27f5f84a62ced
Sagiruddin Mondal <sagir.sagir@gmail.com>
2019-03-15 19:45:43 +0530

    slice update

--- main/slice.go
+++ main/slice.go
@@ -24,6 +24,17 @@ func main() {
 	m := k[:3]
 	fmt.Println(m)
 
-	
+	p := []string{"Sagir", "Aziz", "Atanu", "Anish"}
+	fmt.Println(p)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println(twoD)
 
 }
